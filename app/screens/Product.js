1a9d6bfa183644b9623ad065a98558ca86a9701a
Gualotuña Amaguaya Brayan Patricio <brayan.gualotuna09@gmail.com>
2025-08-14 19:44:44 -0500

    actividad 2 completa

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,8 +1,12 @@
 import {View, Text, StyleSheet, Button} from 'react-native'
 
+
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
         <Text>Bienvenidos a mi tienda, soy Brayan</Text>
+        <Text style={styles.producto1}>Papitas</Text>
+        <Text style={styles.producto2}>Cueritos</Text>
+        <Text style={styles.producto3}>CocaCola</Text>
         <Button
             title='HOME'
             onPress={()=>{
@@ -16,8 +20,26 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: 'lightblue',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    producto1:{
+        backgroundColor: 'lightgreen',
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+    },
+    producto2:{
+        backgroundColor: 'lightyellow',
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+    },
+    producto3:{
+        backgroundColor: 'lightcoral',
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+    }
   });
