f16b8b540471b6c591a3f3767d1bdb33a642bd64
Gualotuña Amaguaya Brayan Patricio <brayan.gualotuna09@gmail.com>
2025-08-14 19:30:51 -0500

    activiadad 2 p1

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,7 +2,7 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text>Bienvenidos a mi tienda, soy Brayan</Text>
         <Button
             title='HOME'
             onPress={()=>{
